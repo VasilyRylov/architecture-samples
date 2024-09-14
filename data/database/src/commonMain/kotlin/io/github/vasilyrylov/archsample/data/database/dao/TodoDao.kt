b42783750a830c1dd794d7dd192bd9c7668d1fc5
Vasily Rylov <vasiliyrylov@yandex.com>
2024-09-14 08:38:56 +0500

    Use upsert for save todo

--- data/database/src/commonMain/kotlin/io/github/vasilyrylov/archsample/data/database/dao/TodoDao.kt
+++ data/database/src/commonMain/kotlin/io/github/vasilyrylov/archsample/data/database/dao/TodoDao.kt
@@ -1,15 +1,14 @@
 package io.github.vasilyrylov.archsample.data.database.dao
 
 import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Upsert
 import io.github.vasilyrylov.archsample.data.database.entity.TodoEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TodoDao {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Upsert
     suspend fun save(item: TodoEntity)
 
     @Query("DELETE FROM TodoEntity WHERE id = :id")
