82b65302fef88a78e5dc31d48c8d34c1e008d4ea
Vasily Rylov <vasiliyrylov@yandex.com>
2024-10-07 09:47:43 +0500

    Component dependencies as kotlin-inject parent component (#14)

--- feature/todo/todo-component/src/commonMain/kotlin/io/github/vasilyrylov/archsample/feature/todo/component/di/TodoFlowDIComponent.kt
+++ feature/todo/todo-component/src/commonMain/kotlin/io/github/vasilyrylov/archsample/feature/todo/component/di/TodoFlowDIComponent.kt
@@ -1,12 +1,9 @@
 package io.github.vasilyrylov.archsample.feature.todo.component.di
 
 import com.arkivanov.essenty.instancekeeper.InstanceKeeper
-import io.github.vasilyrylov.archsample.common.domain.interfaces.IAuthorizedUserRepository
 import io.github.vasilyrylov.archsample.common.domain.interfaces.ITodoRepository
 import io.github.vasilyrylov.archsample.common.ui.navigation.RouterHolder
-import io.github.vasilyrylov.archsample.data.database.dao.TodoDao
 import io.github.vasilyrylov.archsample.feature.todo.component.api.ITodoComponentDependencies
-import io.github.vasilyrylov.archsample.feature.todo.domain.api.ILogoutUseCase
 import io.github.vasilyrylov.archsample.feature.todo.ui.api.ITodoFlowRouter
 import io.github.vasilyrylov.archsample.todo.data.repository.TodoRepository
 import me.tatarka.inject.annotations.Component
@@ -14,22 +11,13 @@ import me.tatarka.inject.annotations.Provides
 
 @Component
 abstract class TodoFlowDIComponent(
-    private val dependencies: ITodoComponentDependencies
+    @Component val dependencies: ITodoComponentDependencies
 ) : InstanceKeeper.Instance {
 
     private val routerHolder = RouterHolder<ITodoFlowRouter>()
 
     @Provides
-    fun getLogoutUseCase(): ILogoutUseCase = dependencies.logoutUseCase
-
-    @Provides
     fun getRouterHolder(): RouterHolder<ITodoFlowRouter> = routerHolder
-
-    @Provides
-    fun getAuthorizedUserRepository(): IAuthorizedUserRepository = dependencies.authorizedUserRepository
-
-    @Provides
-    fun getTodoDao(): TodoDao = dependencies.todoDao
 
     val TodoRepository.bind: ITodoRepository
         @Provides get() = this
